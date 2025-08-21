f291c99a49d9da418d0b9f3fd390542b6e7ddab0
Adrian Hjertholm Voldseth <adrian@nordicneurolab.com>
2025-08-21 09:57:01 +0200

    fixed pulse session

--- src/linux/nnl_audio.cpp
+++ src/linux/nnl_audio.cpp
@@ -1,50 +1,30 @@
 #include <nnl_audio.h>
-#include <pulse/pulseaudio.h>
+#include <pulse_session.h>
 #include <string>
 
 
-std::string dev;
+std::unique_ptr<nnl_audio::PulseSession> audioSession;
 
 
 int nnl_audio::InitializeAudioSession(const std::string& deviceId)
 {
-    dev = deviceId;
+    InitializeAudioSession();
     return 0;
 }
 
 int nnl_audio::InitializeAudioSession()
 {
-    return InitializeAudioSession("");
+    audioSession = std::make_unique<nnl_audio::PulseSession>();
+    audioSession->Initialize();
+    return 0;
 }
+
+
 
 int nnl_audio::SetSessionVolume(float volume)
 {
-    pa_mainloop* mainloop = pa_mainloop_new();
-    pa_context* context = pa_context_new(pa_mainloop_get_api(mainloop), "SetVolumeApp");
-    pa_context_connect(context, nullptr, PA_CONTEXT_NOFLAGS, nullptr);
-
-    while (true) 
-    {
-        pa_mainloop_iterate(mainloop, 1, nullptr);
-        pa_context_state_t state = pa_context_get_state(context);
-        if (state == PA_CONTEXT_READY) break;
-        if (state == PA_CONTEXT_FAILED || state == PA_CONTEXT_TERMINATED) 
-        {
-            pa_context_unref(context);
-            pa_mainloop_free(mainloop);
-            return -1;
-        }
-    }
-
-
-    pa_context_set_sink_volume_by_name(context, dev.c_str(), pa_sw_volume_from_linear(volume), nullptr, nullptr);
-
-
-    pa_context_disconnect(context);
-    pa_context_unref(context);
-    pa_mainloop_free(mainloop);
-
-    return 0;
+    if (!audioSession) return -1;
+    return audioSession->SetVolume(volume);
 }
 
 
