61038159e1feca7c18c39f0cafecf8e91c6af8b5
Adrian Hjertholm Voldseth <adrian@nordicneurolab.com>
2025-09-05 10:11:26 +0200

    debug

--- src/linux/loopback_stream.h
+++ src/linux/loopback_stream.h
@@ -36,7 +36,7 @@ namespace pulse
         static void StartLoopbackCB(pa_context* c, void* userdata);
         static void ReadLoopbackCB(pa_stream* s, size_t length, void* userdata);
         static void SourceInfoCB(pa_context* c, const pa_source_info* info, int eol, void* userdata);
-        static void StopCB(int signum);
+
 
 
         std::unique_ptr<std::thread> m_thread;
@@ -49,8 +49,7 @@ namespace pulse
         static std::string m_sourceName;
         static std::string m_sinkName;
         static pa_sample_spec m_sampleSpec;
-        std::atomic<bool> m_stopRequested = false;
-        std::atomic<bool> m_isRunning = false;
+        volatile bool m_isRunning = false;
 
     };
 
